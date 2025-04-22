857b36541b5e0d3a9de78e9a0085cca9ba92fdb9
Dylan Fitch <kkarl30@gmail.com>
2025-04-22 00:29:55 -0400

    added styling to cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -10,7 +10,7 @@ export function Card({dataObj}){
     return (
       <div className="card">
         <img src={dataObj.imgSrc}></img>
-        <h2>{dataObj.name}</h2>
+        <h2 className="card-header">{dataObj.name}</h2>
       </div>
     );
 
