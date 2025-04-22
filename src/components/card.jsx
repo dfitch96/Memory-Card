a6d49753e3d5a13145662104a6a66be788572409
Dylan Fitch <kkarl30@gmail.com>
2025-04-22 11:52:28 -0400

    capitalized pokemon names on cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,12 +5,11 @@
 
 export function Card({dataObj, onClick}){
 
-  
-
+  const name = dataObj.name[0].toUpperCase() + dataObj.name.slice(1, dataObj.name.length);
     return (
       <div id={dataObj.name} className="card" onClick={onClick}> 
         <img src={dataObj.imgSrc}></img>
-        <h2 className="card-header">{dataObj.name}</h2>
+        <h2 className="card-header">{name}</h2>
       </div>
     );
 
